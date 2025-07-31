9c3fa22956b3e1cf3d6a7914940a662c74ffb656
khasanlee <xasaniskandarov2000@gmail.com>
2025-07-31 21:39:40 +0900

    Using Response Data from POST Requests

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,4 +1,4 @@
-import {  createContext, useReducer } from "react";
+import {  act, createContext, useReducer } from "react";
 
 export const ExpensesContext = createContext({
   expenses: [],
@@ -11,10 +11,10 @@ export const ExpensesContext = createContext({
 function expensesReducer(state, action) {
   switch (action.type) {
     case "ADD":
-        const id = new Date().toISOString() + Math.random().toString();
-        return[{...action.payload, id: id}, ...state ];
+        return[action.payload, ...state ];
     case "SET": 
-        return action.payload
+        const inverted = action.payload.reverse();
+        return inverted
     case "UPDATE":
         const updatableExpenseIndex = state.findIndex(
           (expense) => expense.id === action.payload.id
--- util/http.js
+++ util/http.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const BACKEND_URL =
   "https://expense-tracker-app-14b30-default-rtdb.firebaseio.com";
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+  const id = response.data.name;
+  return id
 }
 
 export async function fetchExpenses() {
